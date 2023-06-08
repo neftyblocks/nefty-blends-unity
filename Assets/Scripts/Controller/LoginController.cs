1ef2ce40d7179313abc72eaf532f75c3b43c49c1
Kristaps <blksmith.nefty@outlook.com>
2023-06-08 12:18:25 +0200

    logincontroller & sendtransactionjs updated

--- Assets/Scripts/Controller/LoginController.cs
+++ Assets/Scripts/Controller/LoginController.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
 
+/// <summary>
+/// LoginController is responsible for handling the login functionality.
+/// </summary>
 public class LoginController : MonoBehaviour
 {
     [SerializeField] SendTransactionJS sendTransactionJS;
 
+    // Calls the LoginAnchor method of SendTransactionJS for anchor login.
     public void LoginAnchor()
     {
         sendTransactionJS.LoginAnchor();
     }
+    // Calls the LoginCloudWallet method of SendTransactionJS for cloud wallet login.
     public void LoginCloudWallet()
     {
         sendTransactionJS.LoginCloudWallet();
--- Assets/Scripts/Javascript/SendTransactionJS.cs
+++ Assets/Scripts/Javascript/SendTransactionJS.cs
@@ -1,8 +1,12 @@
 using System.Runtime.InteropServices;
 using UnityEngine;
 
+/// <summary>
+/// This class is responsible for communicating with the JavaScript environment inside the Plugin/WrapperJS.jslib file 
+/// </summary>
 public class SendTransactionJS : MonoBehaviour, ISendTransactionJS
 {
+    // Import JavaScript functions from the internal scope Plugin/WrapperJS.jslib file.
     [DllImport("__Internal")]
     private static extern void SubmitBlend(int blendId, string[] assetIds, string[] contractNames, string[] tokenSymbol, string[] tokenQuantity, int ftCount, int assetCount);
 
@@ -22,28 +26,37 @@ public class SendTransactionJS : MonoBehaviour, ISendTransactionJS
     [DllImport("__Internal")]
     private static extern void ChangeRPCEndpointJS(string rpcLink);
 
+
+    // This method calls the JS function to send a secured blend transaction.
     public void SendSecuredBlendTransaction(int blendId, string[] assetIds, string[] contractNames, string[] tokenSymbol, string[] tokenQuantity, int ftCount, int assetCount, string[] protectedAssets, int protectedAssetsCount)
     {
         SendSecuredBlend(blendId, assetIds, contractNames, tokenSymbol, tokenQuantity, ftCount, assetCount, protectedAssets, protectedAssetsCount);
     }
+
+    // This method calls the JS function to send a blend transaction.
     public void SendBlendTransaction(int blendId, string[] assetIds, string[] contractNames, string[] tokenSymbol, string[] tokenQuantity, int ftCount, int assetCount)
     {
         SubmitBlend(blendId, assetIds, contractNames, tokenSymbol, tokenQuantity, ftCount, assetCount);
     }
 
+    // This method calls the JS function to login to an anchor through UAL.
     public void LoginAnchor()
     {
         LoginAnchorJS();
     }
+    // This method calls the JS function to login to a cloud wallet through UAL.
     public void LoginCloudWallet()
     {
         LoginCloudWalletsJS();
     }
 
+    // This method changes the RPC endpoint by calling a JS function.
     public void ChangeRPCEndpoint(string rpcLink)
     {
         ChangeRPCEndpointJS(rpcLink);
     }
+
+    // This method calls a JS function to check the eligibility for blend protection.
     public void IsBlendProtectionEligible(int securityId)
     {
         IsBlendProtectionEligibleJS(securityId);
