24b1159b7d166810efa7a9647117f7f58dde86a1
Kristaps <blksmith.nefty@outlook.com>
2023-06-08 13:55:11 +0200

    Update WhitelistUI.cs

--- Assets/Scripts/UI/WhitelistUI.cs
+++ Assets/Scripts/UI/WhitelistUI.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// The WhitelistUI class manages the whitelist UI element and provides methods to display or remove the whitelist warning.
+/// </summary>
 public class WhitelistUI : MonoBehaviour
 {
       
